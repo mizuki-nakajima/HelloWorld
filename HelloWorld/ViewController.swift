88b9ef6e5905d737a0353306f8badedb8a50c990
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-02-19 13:04:47 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nakajima Mizuki on 2019/02/19.
+//  Copyright © 2019 Nakajima Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
